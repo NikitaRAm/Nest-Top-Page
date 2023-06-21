25afa10e9919c8466b9446be240c28bae67eaa41
Romanovich <nik.romanovich@bk.ru>
2023-06-21 11:33:09 +0300

    added top-page logic

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,20 +1,23 @@
-import { Body, Controller, Post, Param, Get, Delete, Patch, HttpCode, NotFoundException, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, Param, Get, Delete, Patch, HttpCode, NotFoundException, UsePipes, ValidationPipe, UseGuards } from '@nestjs/common';
 import { ProductModel } from './product.model';
 import { FindProductDto } from './dto/find-product.dto';
 import { CreateProductDto } from './dto/create-product.dto';
 import { ProductService } from './product.service';
 import { PRODUCT_NOT_FOUND_ERROR } from './product.constants';
 import { IdValidationPipe } from 'src/pipes/ad-validation.pipe';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('product')
 export class ProductController {
 	constructor(private readonly productService: ProductService) {}
 
+	@UseGuards(JwtAuthGuard)
 	@Post('create')
 	async create(@Body() dto: CreateProductDto) {
 		return this.productService.create(dto);
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Get(':id')
 	async get(@Param('id', IdValidationPipe) id: string) {
 		const product = await this.productService.findById(id);
@@ -24,6 +27,7 @@ export class ProductController {
 		return product;
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Delete(':id')
 	async delete(@Param('id', IdValidationPipe) id: string) {
 		const deletedProduct = await this.productService.deleteById(id);
@@ -32,6 +36,7 @@ export class ProductController {
 		}
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Patch(':id')
 	async patch(@Param('id', IdValidationPipe) id: string, @Body() dto: ProductModel) {
 		const updatedProduct = await this.productService.updateById(id, dto);
